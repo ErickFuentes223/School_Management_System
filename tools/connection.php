6eea9edf44b112feff60a9ac605a7581716a9568
ErickFuentes223 <erickfuna2020@gmail.com>
2023-07-25 20:48:12 -0600

    UI del login

---
+++ tools/connection.php
@@ -0,0 +1,14 @@
+<?php 
+    $host = "localhost";
+    $user = "root";
+    $passwd = "";
+    $bd = "school";
+    $con = mysqli_connect($host,$user,$passwd,$bd);
+    $conex = new mysqli($host,$user,$passwd,$bd);
+    $conex -> set_charset("utf8");
+    if(!$con){
+        die("!MISTAKE! - The connection has not been established");
+    }
+    return $con;
+
+    ?>
